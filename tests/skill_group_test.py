57e7803ddac5ef71f0dbc5c35f83aca26c0066f0
LGXprod <mattghanno@gmail.com>
2024-06-14 13:36:20 +1000

    Added unit test to ensure no removed skills are in job population

--- tests/skill_group_test.py
+++ tests/skill_group_test.py
@@ -27,6 +27,17 @@ class TestSkillGroup(unittest.TestCase):
         self.skill_names = self.skill_group.skill_names
         self.job_population = get_job_population(self.skill_group)
 
+    def test_no_removed_skills(self):
+        is_no_removed_skills = True
+
+        for job in self.job_population:
+            for skill in job.skills:
+                if skill.name in self.skill_group.removed_skills:
+                    is_no_removed_skills = False
+                    break
+
+        self.assertTrue(is_no_removed_skills)
+
     def test_equal_matrices(self):
         is_equal = True
 
@@ -35,7 +46,7 @@ class TestSkillGroup(unittest.TestCase):
                 self.skill_group.matrix,
                 get_skill_group_matrix(self.job_population, self.skill_names),
             )
-            
+
             if not is_equal:
                 break
 
