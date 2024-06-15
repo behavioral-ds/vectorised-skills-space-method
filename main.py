8d6dbd17eb502cddc6178cca9927c862f2d4a7e1
LGXprod <mattghanno@gmail.com>
2024-06-16 01:09:17 +1000

    Updated main to manually run baseline and v2 skill set similarity implementation

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+import numpy as np
+
 from SkillGroup import SkillGroup
 from utils.get_random_skill_group import get_random_skill_group
 from utils.MatrixSubsetIndexes import MatrixSubsetIndexes
 from SkillSimCalculatorBaseline import SkillSimCalculatorBaseline, get_job_population
 from SkillSimCalculatorV2 import SkillSimCalculatorV2
+from utils.get_skill_group_matrix import get_skill_group_matrix
 
 
 def main():
@@ -12,32 +15,16 @@ def main():
     skill_sim_calc = SkillSimCalculatorBaseline(
         job_population, skills=skill_group.skill_names
     )
-    cooccurrence_matrix_1 = skill_sim_calc.cooccurrence_matrix()
-    print(cooccurrence_matrix_1.shape)
-    print(cooccurrence_matrix_1)
-
-    print()
-
     skill_sim_calc_2 = SkillSimCalculatorV2(skill_group)
-    cooccurrence_matrix_2 = skill_sim_calc_2.cooccurrence_matrix()
-    print(cooccurrence_matrix_2.shape)
-    print(cooccurrence_matrix_2)
 
-    # print("Number of jobs:", len(job_population))
-
-    # random_jobs_1 = [
-    #     random.choice(job_population) for _ in range(len(job_population) // 6)
-    # ]
-    # random_jobs_2 = [
-    #     random.choice(job_population) for _ in range(len(job_population) // 3)
-    # ]
-
-    # similarity_score = skill_sim_calc.skill_set_similiarity(
-    #     random_jobs_1, random_jobs_2
-    # )
-
-    # print("Skill Set Similarity Score:", similarity_score)
-
+    matrix_subset_1 = MatrixSubsetIndexes((0, 4))
+    matrix_subset_2 = MatrixSubsetIndexes([len(job_population) - i for i in range(1, 6)])
+    
+    # print(np.array_equal(get_skill_group_matrix(job_population[:11], skill_group.skill_names), skill_group.matrix[:11]))
+    # print(np.array_equal(get_skill_group_matrix(job_population[-10:], skill_group.skill_names), skill_group.matrix[-10:]))
+    
+    print("baseline:", skill_sim_calc.skill_set_similiarity(job_population[:5], job_population[-5:]))
+    print("v2:", skill_sim_calc_2.skill_set_similiarity(matrix_subset_1, matrix_subset_2))
 
 if __name__ == "__main__":
     main()
