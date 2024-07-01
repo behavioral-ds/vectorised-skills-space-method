5c7df8198ecb4d3d63fec257891ebf4c67ee51c6
LGXprod <mattghanno@gmail.com>
2024-07-01 15:09:41 +1000

    Renamed an import to get_random_skills as that is what it has been updated to

--- tests/skill_set_similarity_test.py
+++ tests/skill_set_similarity_test.py
@@ -5,7 +5,7 @@ import time
 import numpy as np
 import cupy as cp
 
-from utils.get_random_skill_group import get_random_skill_group
+from utils.get_random_skills import get_random_skill_group
 from utils.MatrixSubsetIndexes import MatrixSubsetIndexes
 from SkillSimCalculatorBaseline import get_job_population, SkillSimCalculatorBaseline
 from SkillSimCalculatorV2 import SkillSimCalculatorV2
