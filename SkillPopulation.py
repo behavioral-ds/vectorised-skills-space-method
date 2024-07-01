ee53c840204e11185fbf2d683187b832aa5108fe
LGXprod <mattghanno@gmail.com>
2024-07-01 12:21:39 +1000

    Added skill_sets_metadata property and added subset getters that use filter callback function

--- SkillPopulation.py
+++ SkillPopulation.py
@@ -1,4 +1,4 @@
-from typing import Any, Optional
+from typing import Any, Optional, Callable, Type
 from dataclasses import dataclass
 
 import numpy as np
@@ -7,7 +7,7 @@ from utils.MatrixSubsetIndexes import MatrixSubsetIndexes
 
 
 @dataclass
-class SkillSet:
+class SkillSetMetadata:
     id: Optional[str] = None
     id_source: Optional[str] = None
     properties: Optional[dict] = None
@@ -21,15 +21,73 @@ class SkillGroup:
 
 class SkillPopulation:
     matrix: np.ndarray[Any, np.dtype[np.int8]]  # matrix is a 2D array of only 1s and 0s
-    skill_groups_subset: list[tuple[SkillGroup, MatrixSubsetIndexes]]
-    skill_names: list[str]
-    removed_skills: set[str]
+    skill_group_subsets: list[
+        tuple[SkillGroup, MatrixSubsetIndexes]
+    ]  # skill is 1-M with rows in matrix
+    skill_sets_metadata: list[SkillSetMetadata | None]
+    skill_names: list[str]  # 1-1 for each matrix column
+    removed_skills: set[
+        str
+    ]  # skills removed from original input (did not reach threshold frequency)
 
     def __init__(
         self,
         skill_group_skills: dict[str, list[list[str]]],
         skill_freq_threshold: int = 5,
     ):
+        skill_names, removed_skills = self.__filter_skills_by_threshold(
+            skill_group_skills, skill_freq_threshold
+        )
+        self.skill_names = skill_names
+        self.removed_skills = removed_skills
+
+        num_unique_skills = len(self.skill_names)
+
+        job_skill_matrix = []
+        self.skill_group_subsets = []
+        self.skill_sets_metadata = []
+        row_index = 0
+
+        for skill_group_id, skill_group_data in skill_group_skills.items():
+            skill_group, skill_sets = self.__get_skill_group_with_sets(
+                skill_group_id, skill_group_data
+            )
+            skill_vectors = []
+            num_skill_sets_removed = 0
+
+            for skill_set_data in skill_sets:
+                skill_set_metadata, skills = self.__get_skill_set_with_metadata(
+                    skill_set_data
+                )
+                has_skill = False
+                skill_vector = np.zeros(num_unique_skills)
+
+                for skill in skills:
+                    if skill not in self.removed_skills:
+                        has_skill = True
+                        skill_vector[self.skill_names.index(skill)] = 1
+
+                if has_skill:
+                    skill_vectors.append(skill_vector)
+                    self.skill_sets_metadata.append(skill_set_metadata)
+                else:
+                    num_skill_sets_removed += 1
+
+            if len(skill_vectors) != 0:
+                end_subset_index = row_index + len(skill_sets) - num_skill_sets_removed
+                self.skill_group_subsets.append(
+                    skill_group, MatrixSubsetIndexes(row_index, end_subset_index)
+                )
+                row_index = end_subset_index + 1
+                job_skill_matrix += skill_vectors
+
+        self.matrix = np.array(job_skill_matrix, dtype=np.int8)
+
+    def __filter_skills_by_threshold(
+        self,
+        skill_group_skills: dict[str, list[list[str]]],
+        skill_freq_threshold: int = 5,
+    ) -> tuple[list[str], list[str]]:
         # counting number of occurrences for each skill
         skill_frequency: dict[str, int] = {}
 
@@ -48,42 +106,63 @@ class SkillPopulation:
         for skill in removed_skills:
             del skill_frequency[skill]
 
-        self.skill_names = list(skill_frequency.keys())
+        return list(skill_frequency.keys()), removed_skills
 
-        num_unique_skills = len(self.skill_names)
+    def __get_skill_group_with_sets(
+        self, skill_group_id: str, skill_group_data: list | dict
+    ) -> tuple[SkillGroup, list[list[str]]]:
+        if skill_group_data is list:
+            return (SkillGroup(skill_group_id), skill_group_data)
 
-        job_skill_matrix = []
-        self.skill_groups_subset = []
-        row_index = 0
+        return (
+            SkillGroup(
+                skill_group_id,
+                {
+                    key: value
+                    for key, value in skill_group_data.items()
+                    if key != "skill_sets"
+                },
+            ),
+            skill_group_data["skill_sets"],
+        )
 
-        for skill_group_id, skill_sets in skill_group_skills.items():
-            skill_group = SkillGroup(skill_group_id)
-            num_skill_sets_removed = 0
-            skill_vectors = []
+    def __get_skill_set_with_metadata(
+        self, skill_set_data: list | dict
+    ) -> tuple[SkillSetMetadata | None, list[str]]:
+        if type(skill_set_data) is list:
+            return (None, skill_set_data)
 
-            for skill_set in skill_sets:
-                skills = skill_set if type(skill_set) is list else skill_set["skills"]
+        id = skill_set_data["id"] if "id" in skill_set_data else None
+        id_source = (
+            skill_set_data["id_source"] if "id_source" in skill_set_data else None
+        )
+        properties = (
+            skill_set_data["properties"] if "properties" in skill_set_data else None
+        )
 
-                has_skill = False
-                skill_vector = np.zeros(num_unique_skills)
+        return SkillSetMetadata(id, id_source, properties), skill_set_data["skills"]
 
-                for skill in skills:
-                    if skill not in removed_skills:
-                        has_skill = True
-                        skill_vector[self.skill_names.index(skill)] = 1
+    def get_matrix_subset_by_sg(
+        self, filter_func: Callable[[Type[SkillGroup]], bool]
+    ) -> MatrixSubsetIndexes:
+        matrix_subset = None
 
-                if has_skill:
-                    skill_vectors.append(skill_vector)
+        for skill_group, group_matrix_subset in self.skill_group_subsets:
+            if filter_func(skill_group):
+                if matrix_subset is None:
+                    matrix_subset = group_matrix_subset
                 else:
-                    num_skill_sets_removed += 1
+                    matrix_subset += group_matrix_subset
 
-            if len(skill_vectors) != 0:
-                end_subset_index = row_index + len(skill_sets) - num_skill_sets_removed
-                self.skill_groups_subset.append(
-                    skill_group, MatrixSubsetIndexes(row_index, end_subset_index)
-                )
-                row_index = end_subset_index + 1
-                job_skill_matrix += skill_vectors
+        return matrix_subset
 
-        self.matrix = np.array(job_skill_matrix, dtype=np.int8)
-        self.removed_skills = removed_skills
+    def get_matrix_subset_by_sg(
+        self, filter_func: Callable[[Type[SkillSetMetadata]], bool]
+    ) -> MatrixSubsetIndexes:
+        indexes = []
+
+        for i, skill_set_metadata in enumerate(self.skill_sets_metadata):
+            if filter_func(skill_set_metadata):
+                indexes.append(i)
+
+        return MatrixSubsetIndexes(indexes)
