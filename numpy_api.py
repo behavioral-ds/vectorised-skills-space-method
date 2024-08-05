3c79b376b93ad4eb564c9f23d675d491d434b8a4
LGXprod <mattghanno@gmail.com>
2024-08-05 14:31:16 +1000

    Updated to make exception control flow easier to read

--- numpy_api.py
+++ numpy_api.py
@@ -1,16 +1,18 @@
 import numpy as np
 
+
 def get_numpy_api() -> np:
     try:
         import cupy as cp
 
         return cp, cp.ndarray  # type: cp
     except ImportError:
-        import mlx.core as mx
+        try:
+            import mlx.core as mx
 
-        return mx, mx.ndarray  # type: mx
-    finally:
-        return np, np.ndarray  # type: np
+            return mx, mx.ndarray  # type: mx
+        except:
+            return np, np.ndarray  # type: np
 
 
 xp, ndarray = get_numpy_api()
